8becd6ce363d0f5f354914e6f35e44f491769a9b
johannb16 <johannb16@skel.ru.is>
2017-09-03 23:46:32 +0000

    initializing msg

--- src/tftpd.c
+++ src/tftpd.c
@@ -6,15 +6,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Define OP Codes.
+#define RRQ 1
+#define WRQ 2
+#define DATA 3
+#define ACK 4
+#define ERROR 5
+
+int getOpcode(char* message){
+    return message[1];
+}
+
 int main(int argc, char **argv)
 {
-    printf("S T U F F");
     if(argc != 3){
         perror("Invalid arguments!\n");
         exit(EXIT_FAILURE);
     }
+    char* port = argv[1];
 
-
+    int block
     int sockfd;
     struct sockaddr_in server, client;
     char message[512];
@@ -25,13 +36,30 @@ int main(int argc, char **argv)
     server.sin_family = AF_INET;
 
     server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(32000);
+    server.sin_port = htons(atoi(port));
     bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
 
     for (;;) {
         socklen_t len = (socklen_t) sizeof(client);
         ssize_t n = recvfrom(sockfd, message, sizeof(message) - 1,
                              0, (struct sockaddr *) &client, &len);
+        int opCode = getOpcode(message);
+        if(message[1] == 1){
+            if()
+        }
+        else{
+            error[0] = 0;
+            error[1] = 5;
+            error[2] = 0;
+            error[3] = 0;
+            error[4] = 'C';
+            error[5] = 'o';
+            error[6] = 'C';
+            error[7] = 'K';
+            error[8] = 0;
+            sendto(sockfd, error, (size_t) n, 0, (struct  sockaddr *) &client, len);
+        }
+        
 
     }
 }
