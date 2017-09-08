4078330056973ac91d4c157b7a0aa42e14bb012d
johannb16 <johannb16@skel.ru.is>
2017-09-08 10:46:50 +0000

    setting up struck function

--- src/tftpd.c
+++ src/tftpd.c
@@ -15,12 +15,38 @@
 
 #define PACKET_SIZE 516
 
+//the variables needed for sending a data pack
 struct data_pack{
     unsigned short opcode;
     unsigned short blocknumber;
     char data[512];
 };
+//setting the opcode number of the data pack and initializing the bloknumber
+void set_data_pack_struct(struct data_pack *data_p, unsigned short blocknr){
+    data_p->opcode = htons(3);
+    data_p->blocknumber = htons(blocknr);
+}
+/*
+void get_filename(){
+    //setja í aðferð fyrir að finna nafnið
+    //finding the file name from the clients request and storing it in an array for further use
+    char* file_name = server_pack + 2;
+    size_t file_name_len = strlen(file_name);
+    size_t argv_len = strlen(argv[2]);
+    char full_path[argv_len + file_name_len + 2];
+    strcpy(full_path, argv[2]);
+    strcpy(full_path + argv_len, "/");
+    strcpy(full_path + argv_len + 1, file_name);
+    full_path[argv_len + file_name_len + 1] = 0;
+    fprintf(stdout, "Full path is: %s\n", full_path); fflush(stdout);
+}/*
+void reading_and_sending_packs(){
 
+}
+void sending_error(){
+	
+}
+*/
 int main(int argc, char **argv)
 {
     // If we receive less than 3 arguments, terminate.
@@ -32,15 +58,13 @@ int main(int argc, char **argv)
 
     fprintf(stdout, "Setting up server nr. %s\n", port); fflush(stdout);
 
-
-    int block;
+    unsigned short blocknr;
     int sockfd;
     struct sockaddr_in server, client;
     //char client_pack[PACKET_SIZE];
     char server_pack[PACKET_SIZE];
     char error[512];
-    char ack[4];
-
+    char ack_buffer[4];
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     memset(&server, 0, sizeof(server));
@@ -57,33 +81,27 @@ int main(int argc, char **argv)
         ssize_t n = recvfrom(sockfd, server_pack, sizeof(server_pack) - 1,
                              0, (struct sockaddr *) &client, &len);
 
-
-        fprintf(stdout, "received something from.... (TODO)\n"); fflush(stdout);
-
+        fprintf(stdout, "received something from.... (TODO:setja inn client info maybe)\n"); fflush(stdout);
 
         if(server_pack[1] == RRQ){ //the msg is a RRQ
-            block = 1;
+            blocknr = 1;
+            //setja í aðferð fyrir að finna nafnið
+            //finding the file name from the clients request and storing it in an array for further use
             char* file_name = server_pack + 2;
-
             size_t file_name_len = strlen(file_name);
-
             size_t argv_len = strlen(argv[2]);
-
             char full_path[argv_len + file_name_len + 2];
-
             strcpy(full_path, argv[2]);
             strcpy(full_path + argv_len, "/");
             strcpy(full_path + argv_len + 1, file_name);
-
-
             full_path[argv_len + file_name_len + 1] = 0;
-
-
             fprintf(stdout, "Full path is: %s\n", full_path); fflush(stdout);
 
+//setja í aðferð til að opna og senda file
             FILE *file;
             file = fopen(full_path, "r");
 
+//setja error í aðferð, samt 7 mismunadni aðferðir...
             if(file == NULL){
                 error[0] = 0;
                 error[1] = 5;
@@ -92,83 +110,67 @@ int main(int argc, char **argv)
                 sprintf(error + 4, "File not found!");
                 sendto(sockfd, error, 19, 0, (struct  sockaddr *) &client, len);
             }
-
+            //transfer loop starts
             while(1){
                 //creating a data pack
                 struct data_pack d_packet;
-                d_packet.opcode = htons(3);
-                d_packet.blocknumber = htons(block);
-                size_t number_of_bytes = fread(d_packet.data, 1, 512, file);
-                sendto(sockfd, (char*)&d_packet, number_of_bytes + 4, 0, (struct  sockaddr *) &client, len);
-                fprintf(stdout, "things have been sent \n"); fflush(stdout);
-                ssize_t ack_received = recvfrom(sockfd, ack, sizeof(ack) - 1,
-                             0, (struct sockaddr *) &client, &len);
-                fprintf(stdout, "ack received\n"); fflush(stdout);
-                /*if(ack[1] != 4){
+                set_data_pack_struct(&d_packet, blocknr);
+				size_t number_of_bytes = fread(d_packet.data, 1, 512, file);
+				sendto(sockfd, (char*)&d_packet, number_of_bytes + 4, 0, (struct  sockaddr *) &client, len);
+				fprintf(stdout, "inside inner loop\n"); fflush(stdout);
+				ssize_t ack_received = recvfrom(sockfd, ack_buffer, sizeof(ack_buffer) - 1,
+				0, (struct sockaddr *) &client, &len);
+
+                /*if(ack_buffer[1] != 4){
                     exit(1);
                     //gera mad error
                 }
-                else if(ack[4] != block){
+                else if(ack_buffer[4] != block){
                     exit(1);
                     //gera mad error
                 }*/
                 while (number_of_bytes == 512){
-                    block++;
-                    d_packet.opcode = htons(3);
-                    d_packet.blocknumber = htons(block);
-                    number_of_bytes = fread(d_packet.data, 1, 512, file);
-                    sendto(sockfd, (char*)&d_packet, number_of_bytes + 4, 0, (struct  sockaddr *) &client, len);
-                    fprintf(stdout, "inside inner loop\n"); fflush(stdout);
-                    ack_received = recvfrom(sockfd, ack, sizeof(ack) - 1,
-                                 0, (struct sockaddr *) &client, &len);
-                    /*if(ack[1] != 4){
+                    blocknr++;
+	                set_data_pack_struct(&d_packet, blocknr);
+	                number_of_bytes = fread(d_packet.data, 1, 512, file);
+	                sendto(sockfd, (char*)&d_packet, number_of_bytes + 4, 0, (struct  sockaddr *) &client, len);
+	                fprintf(stdout, "inside inner loop\n"); fflush(stdout);
+	                ack_received = recvfrom(sockfd, ack_buffer, sizeof(ack_buffer) - 1,
+	                             0, (struct sockaddr *) &client, &len);
+                    /*if(ack_buffer[1] != 4){
                         exit(1);
                         //gera mad error
                     }
-                    else if(ack[4] != block){
+                    else if(ack_buffer[4] != block){
                         exit(1);
                         //gera mad error
                     }*/
                 }
-                block++;
-                d_packet.opcode = htons(3);
-                d_packet.blocknumber = htons(block);
+                blocknr++;
+                set_data_pack_struct(&d_packet, blocknr);
                 number_of_bytes = fread(d_packet.data, 1, 512, file);
                 sendto(sockfd, (char*)&d_packet, number_of_bytes + 4, 0, (struct  sockaddr *) &client, len);
-                fprintf(stdout, "sending last pack\n"); fflush(stdout);
-                ack_received = recvfrom(sockfd, ack, sizeof(ack) - 1,
+                fprintf(stdout, "inside inner loop\n"); fflush(stdout);
+                ack_received = recvfrom(sockfd, ack_buffer, sizeof(ack_buffer) - 1,
                              0, (struct sockaddr *) &client, &len);
-                if(ack[1] != 4){
+                /*if(ack_buffer[1] != 4){
                     exit(1);
                     //gera mad error
                 }
-                else if(ack[4] != block){
+                else if(ack_buffer[4] != block){
                     exit(1);
                     //gera mad error
-                }
+                }*/
                 break;
             }
             fclose(file);
-
-            /*char testfile[513];
-            while(1){
-
-                int nnn = fread(testfile, 1, 512, file);
-                fprintf(stdout, "N = %d\n", nnn); fflush(stdout); 
-                fprintf(stdout, "%s\n", testfile); fflush(stdout);
-                fprintf(stdout, "------------------------------------------\n"); fflush(stdout);
-                if (nnn < 512) break;
-            }
-            fflush(stdout);*/
-
-
 
             /*      ° store info about sender
                     ° start >>> transfer loop <<<*/
         }
         else{ //the msg is not a RRQ
 
-            fprintf(stdout, "Some retard tried something stuid\n"); fflush(stdout);
+            fprintf(stdout, "Some retard tried something stupid\n"); fflush(stdout);
 
             error[0] = 0;
             error[1] = 5;
