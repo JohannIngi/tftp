d75c0a5ae21f0c7904008a52c6380d1c7a13e889
johannb16 <johannb16@skel.ru.is>
2017-09-11 10:34:55 +0000

    code comment & cleanup complete

--- src/tftpd.c
+++ src/tftpd.c
@@ -17,8 +17,6 @@
 #define ACK 4
 #define ERROR 5
 
-#define PACKET_SIZE 516
-
 #define error_code_file_not_found 1
 #define error_code_access_violation 2
 #define error_code_disk_full_or_allocation_exceeded 3
@@ -31,14 +29,13 @@ typedef int bool;
 #define true 1
 #define false 0
 
-
-//the variables needed for sending a data pack
+//the variables needed for sending a data pack are kept in a struct
 struct data_pack{
     unsigned short opcode;
     unsigned short blocknumber;
     char data[512];
 };
-//setting the opcode number of the data pack and initializing the bloknumber
+//setting the opcode number of the data pack and initializing the blocknumber
 void set_data_pack_struct(struct data_pack* data_p, unsigned short blocknr){
     data_p->opcode = htons(3);
     data_p->blocknumber = htons(blocknr);
@@ -67,7 +64,7 @@ void sending_error_pack(int sockfd, struct sockaddr_in* client, unsigned int err
     error_buffer[3] = error_code;
     //length of the error array
     size_t error_len = 0;
-    //setting appropriate error code
+    //setting appropriate error code and matching it to an error message
     switch(error_code)
     {
         case error_code_file_not_found:
@@ -96,7 +93,7 @@ void sending_error_pack(int sockfd, struct sockaddr_in* client, unsigned int err
     }
     //setting length of error array
     error_len = strlen(error_buffer + 4) + 4;
-    //sending error
+    //sending error package
     sendto(sockfd, error_buffer, error_len, 0, (struct  sockaddr *)client, (socklen_t)sizeof(struct sockaddr_in));
 }
 
@@ -111,12 +108,11 @@ int main(int argc, char **argv){
     char* ip_address;
     unsigned short blocknr;
     int sockfd;
-    int error_counter = 0;
+    int error_counter;
     struct sockaddr_in server, client;
-    char server_pack[PACKET_SIZE];
+    char server_pack[516];
     char ack_buffer[4];
     char full_path[100];
-
 
     fprintf(stdout, "Listening to server number: %s\n", port); fflush(stdout);
     //creating and binding a UDP socket
@@ -129,12 +125,13 @@ int main(int argc, char **argv){
     bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
     fprintf(stdout, "Socket set up complete. Waiting for request.\n"); fflush(stdout);
 
-    // server loop
+    // server loop starts
     for (;;) {
-
+    	//creating a timeval variables to use for timeout
 	    struct timeval timeout;
 	    timeout.tv_sec = 0;
 	    timeout.tv_usec = 0;
+	    //setting up setsockopt() for recvfrom()
 	    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
         socklen_t len = (socklen_t) sizeof(client);
@@ -143,18 +140,21 @@ int main(int argc, char **argv){
                              0, (struct sockaddr *) &client, &len);
         //null terminating
         server_pack[n] = '\0';
-
+        //after receiving for the first time timout timer set to 2 seconds
 		timeout.tv_sec = 2;
 		timeout.tv_usec = 100;
 		setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
-        //acquiring the ip address from the client
+        //acquiring the ip address from the client in string form to use in output line 172
         ip_address = inet_ntoa(client.sin_addr);
+        //keeping ip addres as a unsigned long as well to use for comparison inside the inner transfer loop
         unsigned long ip_address_number = client.sin_addr.s_addr;
-        //storing information about the port from client
+        //storing information about the port from client to use for comparison inside the inner transfer loop
         unsigned short original_port = client.sin_port; 
-
+        //is always true except when timeout counter has reached max
         bool data_transfer_running = true;
+        //setting counter
+        error_counter = 0;
         //the message is a RRQ
         if(server_pack[1] == RRQ){ 
             fprintf(stdout, "The request is a read request (RRQ)\n"); fflush(stdout);
@@ -162,8 +162,7 @@ int main(int argc, char **argv){
             blocknr = 1;
             //finding the file name from the clients request and storing it in an array for further use
             file_name = server_pack + 2;
-
-            //checking to see if the file name contains an .. wich is an access violation error
+            //checking to see if the file name contains an ".."" wich is an access violation error
             char * illegal_checker = strstr (file_name, "..");
             if(illegal_checker != NULL){
                 sending_error_pack(sockfd, &client, 2);
@@ -171,10 +170,11 @@ int main(int argc, char **argv){
             }
             //putting toghether file name
             get_filename(file_name, argv[2], full_path);
-            //what is the complete filename and who requested it, ip address and port
+            //Outputting the complete filename and who requested it, ip address and port
             fprintf(stdout, "File requested is: %s. From ip address: %s. Port number: %d\n", full_path, ip_address, original_port); fflush(stdout);
 
             FILE *file;
+            //opening the file
             file = fopen(full_path, "r");
             //file not found villa
             if(file == NULL){
@@ -182,45 +182,43 @@ int main(int argc, char **argv){
                 continue;
             }
             struct data_pack d_packet;
+
             //transfer loop starts
             while(data_transfer_running){
-            	fprintf(stdout, "transfer loop starts \n"); fflush(stdout);
                 //creating a data pack
                 //setting the opcode and blocknumber
                 set_data_pack_struct(&d_packet, blocknr);
                 //reading a total of 512 bytes into a package to send
                 size_t number_of_bytes = fread(d_packet.data, 1, 512, file);
-
                 //sendingng the data package
                 sendto(sockfd, (char*)&d_packet, number_of_bytes + 4, 0, (struct  sockaddr *) &client, len);
+
                 while(true){
                     //receiving ack from client
                     //Getting a value from ack
-                    //fprintf(stdout, "about to receive \n"); fflush(stdout);
                     ssize_t receiver = recvfrom(sockfd, ack_buffer, sizeof(ack_buffer),
                                  0, (struct sockaddr *) &client, &len);
-                    fprintf(stdout, "inner transfer loop starts \n"); fflush(stdout);
-                    //get the blocknumber from ack received
 
+                    //if there is no value from the received ack
                     if(receiver < 0){
-
+                    	//And errno is either EAGAIN or EWOULDBLOCK then the client has timedout
 						if(errno == EAGAIN || errno == EWOULDBLOCK){
-							//if no response, resend the data 5 times, if no answer still...send error
-							fprintf(stdout, "timeout counter going up \n"); fflush(stdout);
+							//if timeout, resend the data 5 times, if no answer still...send error and break to server loop
 							error_counter++;
 							if(error_counter == 5){
 								timeout.tv_sec = 0;
 								timeout.tv_usec = 0;
 								sending_error_pack(sockfd, &client, 0);
+								//in order to reach server loop data_transfer_running has to be false
 								data_transfer_running = false;
 								break;
 							}
+							//resending data package
 							sendto(sockfd, (char*)&d_packet, number_of_bytes + 4, 0, (struct  sockaddr *) &client, len);
 							continue;
 						}
                     	else{
                         	//system error if ack value is < 0 
-                        	fprintf(stdout, "Terminating the program \n"); fflush(stdout);
                         	sending_error_pack(sockfd, &client, 0);
                         	exit(1);
                         }   
@@ -235,12 +233,11 @@ int main(int argc, char **argv){
                         sending_error_pack(sockfd, &client, 5);
                         continue;
                     }
-                    
+                    //get the blocknumber from ack received using a bit manipulation in order to cover the whole 16 bits that could ppossibly store the number
                     unsigned short recv_block = ((unsigned char)ack_buffer[2] << 8) + (unsigned char)ack_buffer[3];
-                    fprintf(stdout, "my block number: %hu. Ack blocknumber: %hu\n", blocknr, recv_block); fflush(stdout);
                     //if ack has not the same block number
                     if(recv_block != blocknr){
-                        //re-send and counter ++
+                        //resend
                         sendto(sockfd, (char*)&d_packet, number_of_bytes + 4, 0, (struct  sockaddr *) &client, len);
                         continue;
                     }
@@ -249,7 +246,6 @@ int main(int argc, char **argv){
                 }
             	//if number of bytes is not 512 then 
             	if(number_of_bytes < 512){
-            		fprintf(stdout, "number of bytes is less than 512\n"); fflush(stdout);
                 	break;
                 }
                 blocknr++;
